ce906c6c98f081d53954fccc0d65ef181d5deb06
crazycoder44 <crazycoder44@gmail.com>
2025-03-26 10:10:53 +0100

    added railway url to CORS allowed origins

--- backend/trakav2/settings.py
+++ backend/trakav2/settings.py
@@ -158,6 +158,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",  # Replace with your frontend URL
+    "https://traka_v2-backend.railway.internal"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
